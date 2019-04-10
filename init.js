8eebe2008efe668ba2ef5ef1b0d255789549acc4
6842H <46144628+6842H@users.noreply.github.com>
2019-04-10 19:07:46 +0800

    Update init.js

--- init.js
+++ init.js
@@ -2,19 +2,19 @@
 //加载CSS，立即生效
 function loadCSS(url){
 	var mlink = document.createElement('link');
-		mlink.type = 'text/css';
-		mlink.rel = 'stylesheet';
-		mlink.href = url;
-		var head = document.getElementsByTagName('head')[0];
-		head.appendChild(mlink);
+	mlink.type = 'text/css';
+	mlink.rel = 'stylesheet';
+	mlink.href = url;
+	var head = document.getElementsByTagName('head')[0];
+	head.appendChild(mlink);
 }
 
 //加载JS，立即生效
 function loadJS(url){
 	var mlink = document.createElement('script');
-		mlink.src = url;
-		var head = document.getElementsByTagName('head')[0];
-		head.appendChild(mlink);
+	mlink.src = url;
+	var head = document.getElementsByTagName('head')[0];
+	head.appendChild(mlink);
 }
 
 xpath_key=9;	//重置获取xpath的快捷键
